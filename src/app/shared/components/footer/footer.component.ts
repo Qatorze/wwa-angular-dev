d8db565f002d8c4e8a5a7ded4f031e8afd0b6119
Steeve MOKOKO <steeve.mokoko14@gmail.com>
2024-12-19 02:40:48 +0100

    Ajout footer non responsive

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faWhatsapp, faInstagram, faFacebook,  } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faWhatsapp = faWhatsapp;
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
 }
